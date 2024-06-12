1360152062ddef8119a55fd2f3ba137061765544
Mohammad Mohid <49339308+mohammadmohid@users.noreply.github.com>
2024-06-12 11:22:15 +0500

    Added helmet package

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: 'https://digitalalumniconnecter.vercel.app', credentials: true })); 
+app.use(cors({ origin: 'http://localhost:5173', credentials: true })); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
