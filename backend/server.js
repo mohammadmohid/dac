18f0691136593c80d498a72a98248c4ffbe28083
Mohammad Mohid <49339308+mohammadmohid@users.noreply.github.com>
2024-06-25 15:15:59 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(helmet());
 app.use(cors({
   origin: 'https://digitalalumniconnecter.vercel.app',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
